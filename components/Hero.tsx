7e988f3729d340357e63edee827c3a71e3e68774
manguriu <brianmiruri983@gmail.com>
2023-06-13 09:29:52 +0300

    footer-hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import {CustomButton} from '@/components/ExportFile'
+import { SocialIcon } from 'react-social-icons';
 
 const Hero = () => {
     const hadleScroll= () => {}
@@ -11,9 +12,19 @@ const Hero = () => {
                 My Name is Brian Manguriu
             </h1>
             <p className='hero__subtitle'> A passionate Web Developer</p>
-            <CustomButton title="Explore Site" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={hadleScroll}/>       
+            <CustomButton title="Explore Site" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={hadleScroll}/>   
+            <div>
+            <SocialIcon url="https://www.linkedin.com/in/brian-manguriu-3b0b07207/"  fgColor='green' bgColor='transparent'/>
+            <SocialIcon url="https://twitter.com/Mainnet14" fgColor='blue' bgColor='transparent'/>
+            <SocialIcon url="https://github.com/Manguriu" fgColor='black' bgColor='transparent' />
+        </div>    
         </div>
-       
+        <div className='hero__image-container'>
+          <div className='hero__image'>
+            <Image src="/alpha2.png" alt=""  fill className='object-contain'/>
+            <div className='hero__image-overlay'/>
+          </div>
+        </div>
     </div>
   )
 }
